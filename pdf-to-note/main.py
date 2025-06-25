78fb1368d3813ab1293ed9b2dd74dd918b6f3850
Dheerapat Tookkane <dheerapat38@gmail.com>
2025-06-26 01:18:44 +0700

    feat: use pdf to note as a script

--- pdf-to-note/main.py
+++ pdf-to-note/main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import typer
 import pymupdf
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -12,7 +13,7 @@ client = OpenAI(
 )
 
 
-def convert_pdf_to_images(pdf_path: str, output_dir: str) -> int:
+def pdf_to_image_converter(pdf_path: str, output_dir: str) -> int:
     """
     Convert PDF pages to images and save them in the specified output directory.
 
@@ -23,21 +24,21 @@ def convert_pdf_to_images(pdf_path: str, output_dir: str) -> int:
     os.makedirs(output_dir, exist_ok=True)
     doc = pymupdf.open(pdf_path)
     for page in doc:
-        pix = page.get_pixmap()  # type: ignore
+        pix = page.get_pixmap(dpi=120)  # type: ignore
         image_path = os.path.join(output_dir, f"page-{page.number}.png")
         pix.save(image_path)
 
     return len(doc)
 
 
-def encode_image(image_path):
+def image_encoder(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-def llm_extract_slide_info(img: str):
+def vllm_extractor(img: str, current_directory: str):
     img_path = os.path.join(current_directory, "img", img)
-    base64_image = encode_image(img_path)
+    base64_image = image_encoder(img_path)
     completion = client.chat.completions.create(
         model=os.getenv("VISION_MODEL_NAME", ""),
         messages=[
@@ -46,7 +47,7 @@ def llm_extract_slide_info(img: str):
                 "content": [
                     {
                         "type": "text",
-                        "text": "Extract the content from the provided document as if you were reading it naturally.",
+                        "text": "Extract the content from the provided document image.",
                     },
                     {
                         "type": "image_url",
@@ -67,13 +68,39 @@ def llm_extract_slide_info(img: str):
     print("Response saved to resp.txt")
 
 
-if __name__ == "__main__":
+def llm_summarizer(file: str, current_directory: str):
+    file_path = os.path.join(current_directory, file)
+    with open(file_path, "rt") as f:
+        completion = client.chat.completions.create(
+            model=os.getenv("TEXT_MODEL_NAME", ""),
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are expert learner, you can summarized information down to a very important piece that really matters, and discard any not useful information.",
+                },
+                {
+                    "role": "user",
+                    "content": f"summarized following piece of information that extracted from document.\n{f.read()}",
+                },
+            ],
+            temperature=0.2,
+        )
+        response_content = completion.choices[0].message.content
+        return response_content
+
+
+def main(pdf_path: str):
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    pdf_path = os.path.join(current_directory, "oop.pdf")
     img_directory = os.path.join(current_directory, "img")
 
-    number_of_pages = convert_pdf_to_images(pdf_path, img_directory)
+    number_of_pages = pdf_to_image_converter(pdf_path, img_directory)
 
     for page in range(number_of_pages):
         img = f"page-{page}.png"
-        llm_extract_slide_info(img)
+        vllm_extractor(img, current_directory)
+
+    print(llm_summarizer("resp.txt", current_directory))
+
+
+if __name__ == "__main__":
+    typer.run(main)
